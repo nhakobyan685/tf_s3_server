240523a1f0644026e1036c47c41b339d86e678c6
nhakobyan685 <118097522+nhakobyan685@users.noreply.github.com>
2023-02-10 17:04:07 +0400

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -1,15 +1,15 @@
-terraform {
-  cloud {
-    organization = "nhakobyan685"
-    workspaces {
-      name = "s3_destroy"
-    }
-  }
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-      version = "4.54.0"
-    }
+ terraform {
+#   cloud {
+#     organization = "nhakobyan685"
+#     workspaces {
+#       name = "s3_destroy"
+#     }
+#   }
+#   required_providers {
+#     aws = {
+#       source = "hashicorp/aws"
+#       version = "4.54.0"
+#     }
   }
   backend "s3" {
     bucket = "nhakobyan685"
